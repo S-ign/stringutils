ac3c0a4d7286d871d9930a41a0abb7424339fb9f
Avery Carty <avery.carty7@gmail.com>
2022-04-04 21:07:52 -0400

    update: Added GetAllSubstrInBetweenTwoStrs

--- stringutils.go
+++ stringutils.go
@@ -4,6 +4,7 @@ import (
 	"crypto/sha256"
 	"encoding/base64"
 	"fmt"
+	"strings"
 	"time"
 )
 
@@ -15,4 +16,30 @@ func GenerateHash(b []byte) (string, error) {
 	h.Write([]byte(fmt.Sprintf("%s%s", string(b), time.Now())))
 	hash := base64.URLEncoding.EncodeToString(h.Sum(nil))
 	return hash, nil
+}
+
+// GetAllSubstrInBetweenTwoStrs returns a slice of substrings that are
+// surrounded by given start and end strings.
+// Ex.
+// str := "Get {{.placeholders}} within a {{.template}} string."
+// substrs := GetAllSubstrInBetweenTwoStrs(str, "{{.", "}}")
+// substrs = []string{"placeholders", "template"}
+func GetAllSubstrInBetweenTwoStrs(str string, startS string, endS string) map[string]string {
+	strS := strings.Split(str, " ")
+	substrs := make(map[string]string)
+	for _, ss := range strS {
+		var result string
+		s := strings.Index(ss, startS)
+		if s == -1 {
+			continue
+		}
+		newS := ss[s+len(startS):]
+		e := strings.Index(newS, endS)
+		if e == -1 {
+			continue
+		}
+		result = newS[:e]
+		substrs[result] = ""
+	}
+	return substrs
 }
